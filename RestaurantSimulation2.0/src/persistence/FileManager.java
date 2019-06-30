a49f0506e6c144474cfb0084906074363d06429e
Mille <Mille@DESKTOP-0BE4567>
2019-06-30 11:06:04 -0500

    horas

---
+++ RestaurantSimulation2.0/src/persistence/FileManager.java
@@ -0,0 +1,31 @@
+package persistence;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class FileManager {
+
+	public static List<String> readFileHour() throws IOException{
+		List<String> listLines;
+		listLines = Files.readAllLines(Paths.get("src/data/hoursToSimulation.csv"));
+		return listLines;		
+	}
+	
+	public static String[] splitLine(String line, String separator){
+		return line.split(separator);
+	}
+
+	public double toDouble(String line) {
+		return Double.parseDouble(line);
+	}
+
+	public void print(List<String> listData) {
+		for (int i = 0; i < listData.size(); i++) {
+			if (listData.get(i) != null) {
+				System.out.println(listData.get(i).toString());
+			}
+		}
+	}
+}
